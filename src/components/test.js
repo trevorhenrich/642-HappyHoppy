ca8610b0f289e395a691387fd02b756314086ce7
Trevor Henrich <trevorhenrich@yahoo.com>
2023-04-13 21:13:30 -0700

    Added Test Component

---
+++ src/components/test.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+// axios.get("http://localhost:3001/api")
+// .then((res)=>{
+//   setData(res.data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+
+function TestComponent() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api")
+  .then((res)=>{
+    console.log(res.data[0].name);
+    setData(res.data[0].name);
+  })
+  .catch((err)=>{
+      console.log(err);
+  });
+  }, []);
+
+  return (
+    <div>
+      {data}
+    </div>
+  );
+}
+
+export default TestComponent;

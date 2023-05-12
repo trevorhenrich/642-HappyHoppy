1e78bd0ff866eb4f95f72325689152a2ae6dc3d3
Trevor Henrich <trevorhenrich@yahoo.com>
2023-05-11 19:43:08 -0700

    Added navbar component file

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Navbar() {
+  const [data, setData] = useState([]);
+
+  return <div>{"Navbar"}</div>;
+}
+
+export default Navbar;

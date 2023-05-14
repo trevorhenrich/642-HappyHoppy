2630c1035a321299c070be54cf550f6524d2900f
trevorhenrich <trevor@Trevors-MacBook-Air.local>
2023-05-14 01:40:04 -0700

    Fixed navbar labels and icons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Navbar() {
   const [value, setValue] = React.useState(0);
@@ -19,9 +19,9 @@ export default function Navbar() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="My Drinks" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Settings" icon={<AccountCircleIcon />} />
       </BottomNavigation>
     </Box>
   );

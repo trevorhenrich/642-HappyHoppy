1aac6b54ebd501f212d5157e8b4da97ebb9d9016
trevorhenrich <trevor@Trevors-MacBook-Air.local>
2023-05-14 03:22:50 -0700

    Added persistance in navbar between pages. when you click on home, home is highlighted while youre on the home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useNavigate } from "react-router-dom";
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -7,22 +8,25 @@ import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Navbar() {
-  const [value, setValue] = React.useState(0);
+const [value, setValue] = React.useState(0);
+const navigate = useNavigate();
 
   return (
     <Box sx={{ width: 500,  position: "fixed",
       bottom: 0}}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction href='/' label="Home" icon={<HomeIcon />} />
-        <BottomNavigationAction href='/mydrinks' label="My Drinks" icon={<FavoriteIcon />} />
-        <BottomNavigationAction href='/profilesettings' label="Settings" icon={<AccountCircleIcon />} />
-      </BottomNavigation>
+<BottomNavigation
+showLabels
+value={value}
+onChange={(event, newValue) => {
+  setValue(newValue);
+}}
+>
+<BottomNavigationAction onClick={()=>navigate("/")} label="Home" icon={<HomeIcon />} />
+<BottomNavigationAction onClick={()=>navigate("/mydrinks")} label="My Drinks" icon={<FavoriteIcon />} />
+<BottomNavigationAction onClick={()=>navigate("/profilesettings")} label="Settings" icon={<AccountCircleIcon />} />
+</BottomNavigation>
     </Box>
   );
 }
+
+        

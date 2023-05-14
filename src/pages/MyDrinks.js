f1183afcffc81d6c56ef203837c41f3942da280f
Spencer Holsinger <16spholsinger@gmail.com>
2023-05-14 15:28:01 -0700

    super basic drink page

--- src/pages/MyDrinks.js
+++ src/pages/MyDrinks.js
@@ -1,7 +1,68 @@
 import React from "react";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {Grid, TextField, Typography} from "@mui/material";
+import { Box } from "@mui/system";
+import LocalBarIcon from '@mui/icons-material/LocalBar';
 
 export function MyDrinks() {
-  return <>{"MyDrinks Page"}</>;
+  return <>
+  <Box paddingTop={2}>
+            <Grid container 
+            spacing={10}
+            direction="row"
+            alignItems="center"
+            justifyContent="center">
+
+              <Grid item>
+                <LocationOnIcon fontSize="large"/>
+              </Grid>
+
+              <Grid item>
+                <TextField label="Search">
+                  
+                </TextField>
+              </Grid>
+
+              <Grid item>
+                <LocalBarIcon fontSize="large"/>
+              </Grid>
+
+            </Grid>
+          </Box>
+          <Box>
+          <Grid container 
+            direction="row"
+            alignItems="left"
+            justifyContent="center"
+            sx={{ Color : "red"}}>
+              <Grid item padding={5}>
+                <img src = "https://www.tasteofhome.com/wp-content/uploads/2018/03/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg?resize=100,100"></img>
+                <div>Title</div>
+                <div>Location</div>
+                <div>Description</div>
+              </Grid>
+              <Grid item padding={5}>
+                <img src = "https://www.tasteofhome.com/wp-content/uploads/2018/03/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg?resize=100,100"></img>
+                <div>Title</div>
+                <div>Location</div>
+                <div>Description</div>
+              </Grid>
+              <Grid item padding={5}>
+                <img src = "https://www.tasteofhome.com/wp-content/uploads/2018/03/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg?resize=100,100"></img>
+                <div>Title</div>
+                <div>Location</div>
+                <div>Description</div>
+              </Grid>
+              <Grid item padding={5}>
+                <img src = "https://www.tasteofhome.com/wp-content/uploads/2018/03/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg?resize=100,100"></img>
+                <div>Title</div>
+                <div>Location</div>
+                <div>Description</div>
+              </Grid>
+      
+          </Grid>
+          </Box>
+  </>;
 }
 
 export default MyDrinks;

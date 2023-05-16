9f7efe29d23e79d056ae22bf33789af994e92f70
trevorhenrich <trevorhenrich@yahoo.com>
2023-05-16 16:45:10 -0700

    Test

--- src/components/test.js
+++ src/components/test.js
@@ -5,18 +5,6 @@ import axios from 'axios'
 function TestComponent() {
   const [data, setData] = useState([]);
 
-  /*
-  useEffect(() => {
-    axios.get("http://localhost:3001/api")
-  .then((res)=>{
-    console.log(res.data[0].name);
-    setData(res.data[0].name);
-  })
-  .catch((err)=>{
-      console.log(err);
-  });
-  }, []);
-*/ 
 
   return (
     <div>
